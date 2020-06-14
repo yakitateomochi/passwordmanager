d174a33b49276b2651f36553857b7fe3d6d965fc
OchiaiShun <minnanouta.bass@gmail.com>
2020-06-14 15:28:16 +0900

    タイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Password_app'),
+      home: MyHomePage(title: 'パスワード管理'),
     );
   }
 }
